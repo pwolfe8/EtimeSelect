77aa92eb60fc86a6584ae95ea2e0d0d17615a5a1
pwolfe8 <pwolfe854@gmail.com>
2019-07-23 21:01:06 -0400

    got total days and can do pub sub between popup.html and tab. not sure if will use it though

--- Etime_Select_Extension/content.js
+++ Etime_Select_Extension/content.js
@@ -14,3 +14,54 @@ function selectElement(e) {
 
 $(window).click(selectElement);
 
+
+
+
+
+// function isWorkday(num) {
+//     if num. hrs0_i
+// }
+
+var endDateInitVal = '';
+var workdaysDict = [];
+var daysInMonth = 0;
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.greeting == "hello") {
+        
+        var iframe = document.getElementById('unitFrame');
+        if (iframe == null)
+            return;
+        
+        var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+        if (innerDoc == null)
+            return;
+        
+        var totalHours = innerDoc.getElementById('grandTotal2');
+        if (totalHours == null)
+            return;
+        
+        var endingDateBox = innerDoc.getElementById('endingDateSpan');
+        if (endingDateBox == null)
+            return;
+        if (endDateInitVal == '') { // check if init box val has been grabbed yet (only grab once)
+            endDateInitVal = endingDateBox.textContent;
+            daysInMonth = endingDateBox.textContent.split(' ')[1].split(',')[0]; // get number of days in month
+            // create dict of days that are workdays 
+            // for (i=0; i<daysInMonth; i++){
+            //     workdaysDict[i] = isWorkday(i);
+            // }
+
+        }
+        
+        
+        
+        var calculatedSurplusHours = totalHours.textContent;
+        endingDateBox.textContent = endDateInitVal + calculatedSurplusHours;
+        sendResponse({numHours: 'end date: ' + daysInMonth}); //'calculated surplus hours:' + calculatedSurplusHours});
+        
+
+      }
+        
+    });
