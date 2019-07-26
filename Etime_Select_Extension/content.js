68aacb1dd5654c47e3086b5d82c45e3eea245e92
pwolfe8 <pwolfe854@gmail.com>
2019-07-25 22:51:12 -0400

    working functionality on 1 sec interval. will clean up/reduce comutation done each second in next commit

--- Etime_Select_Extension/content.js
+++ Etime_Select_Extension/content.js
@@ -1,67 +1,135 @@
+/* Select the Unselectable Charge Numbers */
 function selectElement(e) {
-    var x = e.clientX, y = e.clientY,
-        elementMouseIsOver = document.elementsFromPoint(x, y);
+    var x = e.clientX, y = e.clientY, elementMouseIsOver = document.elementsFromPoint(x, y);
 
     // get the raw text inside selected element
-    var raw_text = elementMouseIsOver[0].textContent;
-    // console.log(elementMouseIsOver[0]); // for debug
+    var raw_text_element = elementMouseIsOver[0]
+    if (raw_text_element == null)
+        return;
+    var raw_text = raw_text_element.textContent;
     
     // select raw text if not empty and of proper class name
-    if (raw_text.length > 0 && elementMouseIsOver[0].className === "u") {
+    if (raw_text_element.length > 0 && elementMouseIsOver[0].className === "u") {
         window.getSelection().selectAllChildren(elementMouseIsOver[0]);
     }
 }
-
 $(window).click(selectElement);
 
 
+/* Calculate Surplus Hours Worked */
+
+// $("body").on('DOMSubtreeModified', "unitFrameDiv", function() {
+//     console.log('changed');
+// });
+
+function isWorkday(innerDoc, date_num) {
+    if (innerDoc == null)
+        return null;
+    
+    // hrsHeaderTextX is format, where X is (date_num - 1)
+    var id_str = 'hrsHeaderText' + (date_num - 1);
+    var hrsHeaderBox = innerDoc.getElementById(id_str);
+    if (hrsHeaderBox == null)
+        return null;
+
+    var txt = hrsHeaderBox.textContent
+    var dayStr = txt.slice(0, 3);
+    
+    var isTrue = true;
+    if (dayStr == 'Sat' || dayStr == 'Sun')
+        isTrue = false;
+    
+    // console.log("day: " + dayStr + '('+ date_num + '), workday: ' + isTrue);
+    return isTrue;
+
+}
+
+function dayHoursEmpty(innerDoc, date_num) {
+    if (innerDoc == null)
+        return null;
+
+    //DT2_x is format, where X is (date_num - 1)
+    var id_str = 'DT2_' + (date_num - 1);
+    var totalDayHoursBox = innerDoc.getElementById(id_str);
+    if (totalDayHoursBox == null)
+        return null;
+
+    // console.log('total day hours: ' + totalDayHoursBox.textContent);
+    if (totalDayHoursBox.textContent == '')
+        return true;
+    else 
+        return false;
+
+}
+
+
+var monthPeriodEndText = null;
+var daysInMonth = null;
+var currentTotalHours = null;
+var lastTotalHours = null;
 
 
 
-// function isWorkday(num) {
-//     if num. hrs0_i
-// }
 
-var endDateInitVal = '';
-var workdaysDict = [];
-var daysInMonth = 0;
-
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if (request.greeting == "hello") {
+setInterval(function() {
+// chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+    //   if (request.greeting == "hello") {
         
+    // grab inner doc 
         var iframe = document.getElementById('unitFrame');
         if (iframe == null)
             return;
-        
         var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
         if (innerDoc == null)
             return;
         
-        var totalHours = innerDoc.getElementById('grandTotal2');
-        if (totalHours == null)
-            return;
         
-        var endingDateBox = innerDoc.getElementById('endingDateSpan');
-        if (endingDateBox == null)
+        // grab total hours worked so far
+        var currentTotalHoursBox = innerDoc.getElementById('grandTotal2');
+        if (currentTotalHoursBox == null)
             return;
-        if (endDateInitVal == '') { // check if init box val has been grabbed yet (only grab once)
-            endDateInitVal = endingDateBox.textContent;
-            daysInMonth = endingDateBox.textContent.split(' ')[1].split(',')[0]; // get number of days in month
-            // create dict of days that are workdays 
-            // for (i=0; i<daysInMonth; i++){
-            //     workdaysDict[i] = isWorkday(i);
-            // }
+        currentTotalHours = currentTotalHoursBox.textContent;
+        // TODO: check for updates on currentTotalHours and recalculate if so
 
+
+
+        // get box for month period total for num days in month and also to print to it
+        var monthPeriodEndBox = innerDoc.getElementById('endingDateSpan');
+        if (monthPeriodEndBox == null)
+            return;
+        if (monthPeriodEndText == null) { // check if init box val has been grabbed yet (only grab once)
+            monthPeriodEndText = monthPeriodEndBox.textContent;
+            daysInMonth = monthPeriodEndBox.textContent.split(' ')[1].split(',')[0]; // get number of days in month
         }
-        
-        
-        
-        var calculatedSurplusHours = totalHours.textContent;
-        endingDateBox.textContent = endDateInitVal + calculatedSurplusHours;
-        sendResponse({numHours: 'end date: ' + daysInMonth}); //'calculated surplus hours:' + calculatedSurplusHours});
-        
 
-      }
         
-    });
+        // main calculation 
+        var foundLastEnteredDay = false;
+        var numWorkdaysSoFar = 0;
+        for (i=daysInMonth; i>0; i--) {
+            // check if found last day
+            if (!foundLastEnteredDay){
+                // check if that days total hours is empty
+                if (!dayHoursEmpty(innerDoc, i)){
+                    foundLastEnteredDay = true;
+                    numWorkdaysSoFar++;
+                }
+            } 
+            // count workdays until last entered day
+            else {
+                if ( isWorkday(innerDoc, i) ) {
+                    numWorkdaysSoFar++;
+                }
+            }
+        }
+
+        // final calc
+        var calculatedSurplusHours =  currentTotalHours - numWorkdaysSoFar * 8;
+        console.log('work days so far: ' + numWorkdaysSoFar + ', current total: ' + currentTotalHours + ', surplus: ' + calculatedSurplusHours );
+        
+        monthPeriodEndBox.textContent = monthPeriodEndText + calculatedSurplusHours;
+        // sendResponse({numHours: calculatedSurplusHours});
+        
+    //   } // end request greeting == 'hello'
+    // });
+}, 1000); // trigger every 10 seconds
